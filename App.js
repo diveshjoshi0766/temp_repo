1d88115946b5d6302d6b51a32d2b23e27429700d
diveshjoshi0766 <diveshjoshi0766@gmail.com>
2022-06-30 21:57:28 +0530

    reinitialized

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SignInScreen from './screen/SignInScreen';
+import DefaultScreen from './screen/DefaultScreen';
+import ForgotPasswordScreen from './screen/ForgotPasswordScreen';
+import SignUpScreen from './screen/SignUpScreen';
+import PresonalDetailsScreen from './screen/PresonalDetailsScreen';
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Scree</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+ 
+
+
+export default function App({ navigation }) {
+  return (
+  <NavigationContainer>
+    <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown: false}}>
+      <Stack.Screen name="Home" component={PresonalDetailsScreen} />
+      <Stack.Screen name="Details" component={PresonalDetailsScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
+  );
+}
---
+++ screen/SignUpScreen.js
@@ -0,0 +1,345 @@
+import React, { useState } from "react";
+import {
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    TextInput,
+    TouchableOpacity,
+    Dimensions,
+    Platform, 
+    PixelRatio,
+    CheckBox
+} from "react-native";
+import {Avatar} from 'react-native-paper';
+import { StatusBar } from 'expo-status-bar';
+import * as Animatable from 'react-native-animatable';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Feather from 'react-native-vector-icons/Feather';
+// import { SocialIcon } from 'react-native-elements'
+import facebook from '../assets/facebook.png'
+import google from '../assets/google.png'
+import company_logo from '../assets/company_logo.png'
+
+import { useTheme } from 'react-native-paper';
+var {width: SCREEN_WIDTH, height: SCREEN_HEIGHT,} = Dimensions.get('window');
+const scale = SCREEN_WIDTH / 320;
+console.log(SCREEN_HEIGHT)
+console.log(SCREEN_WIDTH)
+export function normalize(size) {
+    const newSize = size * scale 
+    if (Platform.OS === 'ios') {
+      return Math.round(PixelRatio.roundToNearestPixel(newSize))
+    } else {
+      return Math.round(PixelRatio.roundToNearestPixel(newSize)) - 2
+    }
+}
+
+export default function SignUpScreen({navigation}) {
+    const { colors } = useTheme();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("")
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [isSelected, setSelection] = useState(false);
+    const [data, setData] = React.useState({
+        username: '',
+        password: '',
+        check_textInputChange: false,
+        secureTextEntry: true,
+        isValidUser: true,
+        isValidPassword: true,
+    });
+   
+    return (
+        
+    <View style={styles.container}>
+    <Animatable.View 
+        animation="fadeInUpBig"
+        style={[styles.footer, {
+            backgroundColor: "rgb(235 235 235)"
+        }]}
+    >
+
+        <View style={{alignItems: 'center', marginTop: '0'}}>
+            <Image
+                style={styles.stretch}
+                source={require('../assets/logo_comp.png')}
+            />
+        </View>
+
+        <TouchableOpacity>
+            <Text style={{color: '#000000', marginTop:10, textAlign: "center", fontSize:normalize(20)}}>Welcome to <Text style={{fontWeight: 'bold'}}>SurveyOptimus!</Text></Text><Text style={{ textAlign: "center", fontSize:normalize(15)}}>it's quick and easy</Text>
+        </TouchableOpacity>
+
+        <View style={[styles.action, {backgroundColor: '#ffffff'}]}>
+            <FontAwesome 
+                name="user-o"
+                color={colors.text}
+                size={20}
+            />
+            <TextInput 
+                placeholder="Username"
+                placeholderTextColor="#666666"
+                style={[styles.textInput, {
+                    color: colors.text
+                }]}
+                autoCapitalize="none"
+                onChangeText={(val) => textInputChange(val)}
+                onEndEditing={(e)=>handleValidUser(e.nativeEvent.text)}
+            />
+            <Animatable.View
+                animation="bounceIn"
+            >
+                <Feather 
+                    name="check-circle"
+                    color="green"
+                    size={20}
+                />
+            </Animatable.View>
+        </View>
+
+        <Animatable.View animation="fadeInLeft" duration={500}>
+        {/* <Text style={styles.errorMsg}>Username must be 4 characters long.</Text> */}
+        </Animatable.View>
+
+        {/* <Text style={[styles.text_footer, {
+            color: colors.text,
+            marginTop: 35
+        }]}>Password</Text> */}
+        <View style={[styles.action, {backgroundColor: '#ffffff'}]}>
+            <Feather 
+                name="lock"
+                color={colors.text}
+                size={20}
+            />
+            <TextInput 
+                placeholder="Password"
+                placeholderTextColor="#666666"
+                secureTextEntry={data.secureTextEntry ? true : false}
+                style={[styles.textInput, {
+                    color: colors.text
+                }]}
+                autoCapitalize="none"
+                onChangeText={(val) => handlePasswordChange(val)}
+            />
+            <TouchableOpacity
+            >
+                {data.secureTextEntry ? 
+                <Feather 
+                    name="eye-off"
+                    color="grey"
+                    size={20}
+                />
+                :
+                <Feather 
+                    name="eye"
+                    color="grey"
+                    size={20}
+                />
+                }
+            </TouchableOpacity>
+        </View>
+
+        <View style={[styles.action, {backgroundColor: '#ffffff'}]}>
+            <Feather 
+                name="lock"
+                color={colors.text}
+                size={20}
+            />
+            <TextInput 
+                placeholder="Confirm Password"
+                placeholderTextColor="#666666"
+                secureTextEntry={data.secureTextEntry ? true : false}
+                style={[styles.textInput, {
+                    color: colors.text
+                }]}
+                autoCapitalize="none"
+                onChangeText={(val) => handlePasswordChange(val)}
+            />
+            <TouchableOpacity
+            >
+                {data.secureTextEntry ? 
+                <Feather 
+                    name="eye-off"
+                    color="grey"
+                    size={20}
+                />
+                :
+                <Feather 
+                    name="eye"
+                    color="grey"
+                    size={20}
+                />
+                }
+            </TouchableOpacity>
+        </View>
+
+        
+        <View style={[styles.action, {backgroundColor: '#ffffff'}]}>
+            <TextInput 
+                placeholder="First Name"
+                placeholderTextColor="#666666"
+                secureTextEntry={data.secureTextEntry ? true : false}
+                style={[styles.textInput, {
+                    color: colors.text
+                }]}
+                autoCapitalize="none"
+                onChangeText={(val) => handlePasswordChange(val)}
+            />
+        </View>
+
+        
+        <View style={[styles.action, {backgroundColor: '#ffffff'}]}>
+            <TextInput 
+                placeholder="Last Name"
+                placeholderTextColor="#666666"
+                secureTextEntry={data.secureTextEntry ? true : false}
+                style={[styles.textInput, {
+                    color: colors.text
+                }]}
+                autoCapitalize="none"
+                onChangeText={(val) => handlePasswordChange(val)}
+            />
+        </View>
+
+        <View style={styles.container}>
+            <View style={styles.checkboxContainer}>
+                <CheckBox
+                value={isSelected}
+                onValueChange={setSelection}
+                style={styles.checkbox}
+                />
+                <Text style={styles.label}>I accept the <Text style={{color: '#1E96F0'}}>terms and aggrement</Text></Text>
+            </View>
+        </View>
+
+        <View style={styles.button}>
+            <TouchableOpacity
+                onPress={() => navigation.navigate('SignUpScreen')}
+                style={[styles.signIn, {
+                    backgroundColor: '#378C3C',
+                }]}
+            >
+                <Text style={[styles.textSign, {
+                    color: '#fff'
+                }]}>Sign In</Text>
+            </TouchableOpacity>
+        </View>
+            
+        <TouchableOpacity>
+        <Text style={{color: '#000000', marginTop:15, textAlign: "center"}}>Or continue with</Text>
+        </TouchableOpacity>
+        <View style={{alignItems: "center", flexDirection: 'row', justifyContent:'space-around', marginTop: 20}}>
+            <Avatar.Image 
+                source={facebook}
+                size={50}
+            />
+            <Avatar.Image 
+                source={google}
+                size={50}
+            />
+        </View>
+        <TouchableOpacity>
+        <Text style={{color: '#000000', marginTop:15, textAlign: "center", fontSize:normalize(20)}}>Already a member: <Text style={{color: '#1E96F0', fontWeight: 'bold'}}>SIGN IN</Text></Text>
+        </TouchableOpacity>
+    </Animatable.View>
+  </View>
+    );
+  }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, 
+        backgroundColor: 'rgb(235 235 235)',
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'center',
+        width: SCREEN_WIDTH,
+        height: SCREEN_HEIGHT
+    },
+    stretch: {
+        width: SCREEN_WIDTH*0.5,
+        height: SCREEN_WIDTH*0.5,
+        textAlign: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    header: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        paddingHorizontal: 20,
+        paddingBottom: 50
+    },
+    footer: {
+        flex: 1,
+        backgroundColor: 'rgb(235 235 235)',
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+        paddingHorizontal: 20,
+        paddingVertical: 30
+    },
+    text_header: {
+        color: '#fff',
+        fontWeight: 'bold',
+        fontSize: 30
+    },
+    text_footer: {
+        color: '#05375a',
+        fontSize: 18
+    },
+    action: {
+        marginTop: normalize(10),
+        minHeight: 40,
+        flex:1,
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'center',
+        borderRadius: normalize(10),
+        paddingLeft: 3,
+        paddingRight: 3,
+    },
+    actionError: {
+        flexDirection: 'row',
+        marginTop: 10,
+        borderBottomColor: '#FF0000',
+        paddingBottom: 5
+    },
+    textInput: {
+        height: normalize(20),
+        flex: 1,
+        paddingLeft: 10,
+        color: '#05375a',
+    },
+    errorMsg: {
+        color: '#FF0000',
+        fontSize: 14,
+    },
+    button: {
+        alignItems: 'center',
+        marginTop: normalize(5),
+    },
+    signIn: {
+        width: '100%',
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10
+    },
+    textSign: {
+        fontSize: 18,
+        fontWeight: 'bold'
+    },
+    checkboxContainer: {
+        flexDirection: "row",
+        marginBottom: 20,
+    },
+    checkbox: {
+        alignSelf: "center",
+    },
+    label: {
+        margin: 8,
+    },
+  });
